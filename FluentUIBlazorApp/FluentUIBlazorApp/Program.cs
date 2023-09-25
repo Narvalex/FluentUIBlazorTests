3e1594df48523c08ed68dc3a8f1abc1e0b89851d
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2023-09-25 18:57:59 -0400

    Finally able to reproduce the bug

--- FluentUIBlazorApp/FluentUIBlazorApp/Program.cs
+++ FluentUIBlazorApp/FluentUIBlazorApp/Program.cs
@@ -9,6 +9,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddFluentUIComponents();
+builder.Services.AddFluentUIComponents(options =>
+{
+    options.HostingModel = BlazorHostingModel.WebAssembly;
+});
 
 await builder.Build().RunAsync();
