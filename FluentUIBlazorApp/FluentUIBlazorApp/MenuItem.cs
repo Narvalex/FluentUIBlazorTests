def763963224c837a1072e1035870139958a294c
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2023-09-25 14:18:11 -0400

    Added menu. It just works

---
+++ FluentUIBlazorApp/FluentUIBlazorApp/MenuItem.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Fast.Components.FluentUI;
+
+namespace FluentUIBlazorApp;
+
+public class MenuItem
+{
+    public Icon? Icon { get; set; }
+    public string Text { get; set; } = "";
+    public string? Href { get; set; }
+}
---
+++ FluentUIBlazorApp/FluentUIBlazorApp/MenuProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentUIBlazorApp;
+
+public static class MenuProvider
+{
+    private static Action<List<MenuItem>> onMenuChanged = e => { };
+
+    public static void Subscribe(Action<List<MenuItem>> listener) => onMenuChanged = listener;
+
+    public static void Publish(List<MenuItem> items) => onMenuChanged.Invoke(items);
+}
