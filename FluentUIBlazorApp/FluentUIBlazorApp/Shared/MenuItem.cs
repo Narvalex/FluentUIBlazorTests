46bdcb5abcebed7a783d74822a2e62c6968d95a6
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2023-09-27 20:17:46 -0400

    Added routes that crashes

---
+++ FluentUIBlazorApp/FluentUIBlazorApp/Shared/MenuItem.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Fast.Components.FluentUI;
+
+namespace FluentUIBlazorApp.Shared;
+
+public class MenuItem
+{
+    public Icon? Icon { get; set; }
+    public string Text { get; set; } = "";
+    public string Href { get; set; } = "";
+}
